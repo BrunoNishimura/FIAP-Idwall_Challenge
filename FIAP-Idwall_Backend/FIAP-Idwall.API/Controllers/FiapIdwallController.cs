c3427f835864dc12cbb00a9ed399de3abef2b182
Bruno Y. Nishimura <72320100+BrunoNishimura@users.noreply.github.com>
2023-09-30 18:03:48 -0300

    Section 3: EF Core 5 - Adicionando o EF e o DB

--- FIAP-Idwall_Backend/FIAP-Idwall.API/Controllers/FiapIdwallController.cs
+++ FIAP-Idwall_Backend/FIAP-Idwall.API/Controllers/FiapIdwallController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using FIAP_Idwall.API.Data;
 using FIAP_Idwall.API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,38 +14,22 @@ namespace FIAP_Idwall.API.Controllers
     [Route("api/[controller]")]
     public class FiapIdwallController : ControllerBase
     {
-        public IEnumerable<FiapIdwall> _fiapIdwallArray = new FiapIdwall[]{
-            new FiapIdwall() {
-                WantedId = 1,
-                PictureUrl = "https://ws-public.interpol.int/notices/v1/red/2023-14388/images/62681007",
-                Fullname = "XUILI WANG",
-                DateOfBirth = DateTime.Parse("1983/11/17"),
-                Details = "https://www.interpol.int/How-we-work/Notices/Red-Notices/View-Red-Notices#2023-14388"
-            },
-            new FiapIdwall() {
-                WantedId = 2,
-                PictureUrl = "https://ws-public.interpol.int/notices/v1/red/2022-44424/images/62488111",
-                Fullname = "ZHENG LING YAO",
-                DateOfBirth = DateTime.Parse("1980/04/12"),
-                Details = "https://www.interpol.int/How-we-work/Notices/Red-Notices/View-Red-Notices#2022-44424"
-            },
-        };
-
-        public FiapIdwallController()
+        public readonly DataContext _context;
+        public FiapIdwallController(DataContext context)
         {
-
+            _context = context;
         }
 
        [HttpGet]
         public IEnumerable<FiapIdwall> Get()
         {
-           return _fiapIdwallArray;
+           return _context.GetDbFiapIdwalls;
         }
 
         [HttpGet("{id}")]
         public FiapIdwall GetById(int id)
         {
-            return _fiapIdwallArray.FirstOrDefault(evento => evento.WantedId == id); //retorna com os ids
+            return _context.GetDbFiapIdwalls.FirstOrDefault(evento => evento.WantedId == id); //retorna com os ids
         }
 
 
---
+++ FIAP-Idwall_Backend/FIAP-Idwall.API/Data/Migrations/20230930204804_Initial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FIAP_Idwall.API.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GetDbFiapIdwalls",
+                columns: table => new
+                {
+                    WantedId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PictureUrl = table.Column<string>(type: "TEXT", nullable: true),
+                    Fullname = table.Column<string>(type: "TEXT", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Nationality = table.Column<string>(type: "TEXT", nullable: true),
+                    Details = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetDbFiapIdwalls", x => x.WantedId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GetDbFiapIdwalls");
+        }
+    }
+}
